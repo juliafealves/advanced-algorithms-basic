a1a4d6f654962c124fda8914267a896b9a4f03ea
juliafealves <juliafealves>
2018-04-24 21:42:28 -0300

    Solved problem: Little Girl and Game.

--- list-02/little-girl-game.py
+++ list-02/little-girl-game.py
@@ -3,62 +3,27 @@
 # Handle: juliafealves
 # Problem: B. Little Girl and Game
 
-
-def is_palindrome(word):
-    return word == word[::-1]
-
-
-def sum_letter(string):
-        all_letters = set(string)
-
-        for letter in all_letters:
-            amount_letters[letter] = string.count(letter)
-
-
-def get_letter_removed():
-    smaller = min(amount_letters.values())
-
-    for letter, amount in amount_letters.iteritems():
-        if amount == smaller:
-            amount_letters[letter] -= 1
-            if amount_letters[letter] <= 0:
-                amount_letters.pop(letter)
-
-            return letter
-
-
-def remove_letter(word, i, j):
-    substring = word[i:j]
-
-    if len(amount_letters) == 0:
-        sum_letter(substring)
-
-    letter_removed = get_letter_removed()
-    substring = substring.replace(letter_removed, '', 1)
-
-    return word[:i] + substring + word[j:]
-
-
 word = raw_input()
 
-i = 0
-j = len(word) - 1
+all_letters = set(word)
+letter_palindrome = 0
+once_odd = True
 
-player = 1
-amount_letters = {}
+for letter in all_letters:
+    amount_letter = word.count(letter)
 
-while not is_palindrome(word):
-    if word[i] != word[j]:
-        player += 1
-        word = remove_letter(word, i, j + 1)
-        j -= 1
+    if amount_letter % 2 == 0:
+        letter_palindrome += amount_letter
     else:
-        i += 1
-        j -= 1
+        if once_odd:
+            letter_palindrome += amount_letter
+            once_odd = False
+        elif amount_letter - 1 > 0:
+            letter_palindrome += amount_letter - 1
 
-if player % 2 == 0:
-    print "Second"
+player_win = abs(len(word) - letter_palindrome)
+
+if player_win % 2 == 0:
+    print 'First'
 else:
-    print "First"
-
-print word
+    print 'Second'

dfdf7346837894fe3514b027ff9f5dd3def29322
juliafealves <juliafealves>
2018-04-26 01:36:17 -0300

    Solved problem: Tri-du

---
+++ list-02/tri-du.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Author: Júlia Fernandes Alves. <juliafealves@gmail.com>
+# Handle: juliafealves
+# Problem: Tri-du
+
+cards = map(int, raw_input().split())
+print cards[0] if cards[0] == cards[1] else max(cards)

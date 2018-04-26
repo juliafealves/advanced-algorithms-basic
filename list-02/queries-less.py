cb71c24b536944a444ece5f97ba3d8b6bffd20d3
juliafealves <juliafealves>
2018-04-26 01:37:00 -0300

    Trying to solve

--- list-02/queries-less.py
+++ list-02/queries-less.py
@@ -4,31 +4,30 @@
 # Problem: B. Queries about less or equal elements
 
 
-def position_lower(array, index, number, length):
-    while True:
-        if index == length - 1 or array[index + 1] > number:
-            return index + 1
-        else:
-            index += 1
-
-
 def binary_search(array, number, length):
     i, j = 0, length
 
-    while i < j:
+    while abs(i - j) > 1:
         middle = (i + j) / 2
 
         if array[middle] > number:
-            j = middle - 1
+            j = middle
         else:
-            return position_lower(array, middle, number, length)
-    return j + 1
+            i = middle
+
+    if i == 0 and array[i] > number:
+        return 0
+    elif j == length - 1 and array[j] < number:
+        return length
+    else:
+        return j
 
 
 sizes = map(int, raw_input().split())
 a = map(int, raw_input().split())
 b = map(int, raw_input().split())
 a = sorted(a)
+
 visited = {}
 output = ''
 
@@ -36,13 +35,19 @@ for number in b:
     if number in visited.keys():
         output += str(visited[number]) + ' '
         continue
-    elif a[0] > number:
-        visited[number] = 0
-    elif a[-1] <= number:
-        visited[number] = sizes[0]
+    elif a[0] == a[-1] and sizes[0] > 1:
+        if a[0] == number or number > a[-1]:
+            visited[number] = sizes[0]
+        elif number < a[-1]:
+            visited[number] = 0
+
+        output += str(visited[number]) + ' '
+        continue
     else:
         visited[number] = binary_search(a, number, sizes[0])
 
     output += str(visited[number]) + ' '
 
 print output.strip()
+
+

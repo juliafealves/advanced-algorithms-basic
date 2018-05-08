1ca1b6f81cdfb241827e59b8103e47746e381281
juliafealves <juliafealves>
2018-05-07 22:07:11 -0300

    Solved problem: P4 - Misha and Changing Handles

---
+++ list-03/p4.py
@@ -0,0 +1,18 @@
+q = int(raw_input())
+handles = {}
+
+for times in xrange(q):
+    old, new = raw_input().split()
+
+    found = False
+    for older, newer in handles.iteritems():
+        if old == newer:
+            handles[older] = new
+            found = True
+
+    if not found:
+        handles[old] = new
+
+print len(handles)
+for older, newer in handles.iteritems():
+    print '%s %s' % (older, newer)

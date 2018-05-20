e257c46a400c7a0d3fab0a624a08008a0b3d211a
juliafealves <juliafealves>
2018-05-20 15:17:05 -0300

    Solved problem: P1 - Worms

--- list-05/p1.py
+++ list-05/p1.py
@@ -1,39 +1,35 @@
 # -*- coding: utf-8 -*-
 def cumulative_sum(array):
-    cumulative_sum = [0]
+    sum = [array[0]]
 
-    for i in xrange(1, len(array) + 1): cumulative_sum.append(cumulative_sum[i - 1] + array[i - 1])
+    for i in xrange(1, len(array)): sum.append(sum[i - 1] + array[i])
 
-    return cumulative_sum
+    return sum
 
 def binary_search(array, number):
     i, j = 0, len(array)
+    position = -1
 
     while i <= j:
         m = (i + j) / 2
         middle = array[m]
 
-        if middle == number: return m
-        elif middle < number:
-            after = m + 1
+        if middle < number: i = m + 1
+        elif middle >= number:
+            position = m
+            j = m - 1
 
-            if after < len(array) and array[after] >= number: return after
-            i = after
-        elif middle > number:
-            before = m - 1
-
-            if before >= 0 and array[before] == number: return before
-            elif before >= 0 and array[before] < number: return m
-            j = before
-
-    return m
+    return position + 1
 
 n = int(raw_input())
 piles = map(int, raw_input().split())
 m = int(raw_input())
 worms = map(int, raw_input().split())
 
-cumulative_sum = cumulative_sum(piles)
+sum = cumulative_sum(piles)
 
-for worm in worms: print binary_search(cumulative_sum, worm)
+for worm in worms:
+    if worm <= sum[0]: print 1
+    elif sum[-2] < worm <= sum[-1]: print len(sum)
+    else: print binary_search(sum, worm)
 

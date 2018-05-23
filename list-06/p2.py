cdad23663be3860e9775597eb29bcbbf481907ed
juliafealves <juliafealves>
2018-05-23 00:13:43 -0300

    Trying...

--- list-06/p2.py
+++ list-06/p2.py
@@ -1,25 +1,66 @@
 from Queue import Queue
 
-def bfs(x, y, listAdjacent):
-    queue = Queue()
-    vertex = listAdjacent[x][y]
+queue = Queue()
 
-    for i in xrange(x, len(listAdjacent[x])):
-        for j in xrange(y, len(listAdjacent[y])):
-            if (i + 1 < len(listAdjacent[0] and listAdjacent[x][y+1] == 1):
-                queue.put((x, y + 1))
+def bfs(adjacent):
+    count = 0
+    parent_before = (-1, -1)
+    visited = {}
 
+    while not queue.empty():
+        vertex = queue.get()
+        x, y = vertex[0][0], vertex[0][1]
+        vertex_parent = str(x) + str(y)
+        parent = vertex[1]
 
+        if y + 1 < len(adjacent[0]) and adjacent[x][y + 1] == 1:
+            vertex = str(x) + str(y + 1)
 
-listAdjacent = [[0]]
+            try: visited[vertex]
+            except ValueError:
+                visited[vertex] = True
+                queue.put((vertex, vertex_parent))
+
+        if x + 1 < len(adjacent) and y + 1 < len(adjacent[0]) and adjacent[x + 1][y + 1] == 1:
+            queue.put([(x + 1, y + 1), (x, y)])
+
+        if x + 1 < len(adjacent) and adjacent[x + 1][y] == 1:
+            queue.put([(x + 1, y), (x, y)])
+
+        if y - 1 >= 0 and adjacent[x][y - 1] == 1:
+            queue.put([(x, y - 1), (x, y)])
+
+        if x - 1 >= 0 and y - 1 >= 0 and adjacent[x - 1][y - 1] == 1:
+            queue.put([(x - 1, y - 1), (x, y)])
+
+        if x - 1 >= 0 and adjacent[x - 1][y] == 1:
+            queue.put([(x - 1, y), (x, y)])
+
+        if x - 1 >= 0 and y + 1 < len(adjacent[0]) and adjacent[x - 1][y + 1] == 1:
+            queue.put([(x - 1, y + 1), (x, y)])
+
+        if x + 1 < len(adjacent) and y - 1 >= 0 and adjacent[x + 1][y - 1] == 1:
+            queue.put([(x + 1, y - 1), (x, y)])
+
+        adjacent[x][y] = 'x'
+
+        if parent_before != parent:
+            count += 1
+            parent_before = parent
+
+    print count
+    print queue.queue
+    print adjacent
 
 t = int(raw_input())
 for i in xrange(t):
+    adjacent = []
     a, b = map(int, raw_input().split())
 
     for j in xrange(a):
         line = map(int, raw_input().split())
-        listAdjacent.append(line)
+        adjacent.append(line)
 
     x, y = map(int, raw_input().split())
-    bfs(x, y, listAdjacent)
+    queue.put([(x - 1, y - 1), (x - 1, y - 1)])
+    bfs(adjacent)

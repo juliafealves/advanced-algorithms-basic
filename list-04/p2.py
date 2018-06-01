bae92596b9c434b3feabba5d43f0d7a4fe6349f0
juliafealves <juliafealves>
2018-06-01 19:54:37 -0300

    Solved problem: P2 - Marianne and The Twin Cousins

--- list-04/p2.py
+++ list-04/p2.py
@@ -1,43 +1,39 @@
-# https://www.urionlinejudge.com.br/judge/pt/problems/view/1926
-# -*- coding: utf-8 -*-
+# coding: utf-8
 from math import sqrt
 
 def sieve_prime(number):
     primes_sqrt = int(sqrt(number))
-    primes = [True] * (number + 1)
+    primes = [True] * (number)
     primes[0], primes[1] = False, False
 
     for i in xrange(2, primes_sqrt + 1):
         if primes[i]:
-            for j in xrange(i * i, number + 1, i): primes[j] = False
+            for j in xrange(i * i, number, i): primes[j] = False
 
     return primes
 
 def twin_prime():
-    twin_primes.append(3)
+    twin_primes[3] = 1
 
-    for p in xrange(5, MAX + 1, 2):
+    for p in xrange(5, MAX, 2):
         if primes[p] and primes[p + 2]:
-            twin_primes.append(p)
-            twin_primes.append(p + 2)
+            twin_primes[p] = 1
+            twin_primes[p + 2] = 1
 
 def cumulative_sum():
     sum = [twin_primes[0]]
-
     for i in xrange(1, len(twin_primes)): sum.append(sum[i - 1] + twin_primes[i])
 
     return sum
 
 MAX = pow(10, 6) + 1
-twin_primes = []
+twin_primes = [0] * MAX
 primes = sieve_prime(MAX)
 twin_prime()
 
 cumulative_sum = cumulative_sum()
-print twin_primes
-print cumulative_sum
 q = int(raw_input())
 
 for i in xrange(q):
     x, y = map(int, raw_input().split())
-    print len(twin_primes[:max(x , y) + 1])
+    print cumulative_sum[max(x, y)] - cumulative_sum[min(x, y) - 1]

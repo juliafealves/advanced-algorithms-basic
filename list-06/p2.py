46e97c3afdcfb8744fb8cd156cd6cfe0c39ef08d
juliafealves <juliafealves>
2018-06-06 16:57:10 -0300

    Solved problem: P2 - Bicho da Goiaba

--- list-06/p2.py
+++ list-06/p2.py
@@ -7,14 +7,53 @@ def get_children(vertex):
     i, j = vertex
     children = []
 
-    if j < b - 1 and adjacent[i][j + 1] == 1: children.append((i, j + 1))
-    if i < a - 1 and j < b - 1 and adjacent[i + 1][j + 1] == 1: children.append((i + 1, j + 1))
-    if i < a - 1 and adjacent[i + 1][j] == 1: children.append((i + 1, j))
-    if i < a - 1 and j > 0 and adjacent[i + 1][j - 1] == 1: children.append((i + 1, j - 1))
-    if j > 0 and adjacent[i][j - 1] == 1: children.append((i, j - 1))
-    if i > 0 and j > 0 and adjacent[i - 1][j - 1] == 1: children.append((i - 1, j - 1))
-    if i > 0 and adjacent[i - 1][j] == 1: children.append((i - 1, j))
-    if i > 0 and j < b - 1 and adjacent[i - 1][j + 1] == 1: children.append((i - 1, j + 1))
+    child = (i, j + 1)
+    if j < b - 1 and not visited.has_key(child) and adjacent[i][j + 1] == 1:
+        children.append(child)
+        visited[child] = visited[vertex] + 1
+        queue.put(child)
+
+    child = (i + 1, j + 1)
+    if i < a - 1 and not visited.has_key(child) and j < b - 1 and adjacent[i + 1][j + 1] == 1:
+        children.append(child)
+        visited[child] = visited[vertex] + 1
+        queue.put(child)
+
+    child = (i + 1, j)
+    if i < a - 1 and not visited.has_key(child) and adjacent[i + 1][j] == 1:
+        children.append(child)
+        visited[child] = visited[vertex] + 1
+        queue.put(child)
+
+    child = (i + 1, j - 1)
+    if i < a - 1 and j > 0 and not visited.has_key(child) and adjacent[i + 1][j - 1] == 1:
+        children.append(child)
+        visited[child] = visited[vertex] + 1
+        queue.put(child)
+
+    child = (i, j - 1)
+    if j > 0 and not visited.has_key(child) and adjacent[i][j - 1] == 1:
+        children.append(child)
+        visited[child] = visited[vertex] + 1
+        queue.put(child)
+
+    child = (i - 1, j - 1)
+    if i > 0 and j > 0 and not visited.has_key(child) and adjacent[i - 1][j - 1] == 1:
+        children.append(child)
+        visited[child] = visited[vertex] + 1
+        queue.put(child)
+
+    child = (i - 1, j)
+    if i > 0 and not visited.has_key(child) and adjacent[i - 1][j] == 1:
+        children.append(child)
+        visited[child] = visited[vertex] + 1
+        queue.put(child)
+
+    child = (i - 1, j + 1)
+    if i > 0 and j < b - 1 and not visited.has_key(child) and adjacent[i - 1][j + 1] == 1:
+        children.append(child)
+        visited[child] = visited[vertex] + 1
+        queue.put(child)
 
     return children
 
@@ -22,8 +61,6 @@ def count_days():
     while not queue.empty():
         vertex = queue.get()
         graph[vertex] = get_children(vertex)
-
-    print graph.items()
 
 
 q = int(raw_input())
@@ -41,5 +78,6 @@ for i in xrange(q):
     vertex = (x - 1, y - 1)
     queue.put(vertex)
     visited[vertex] = 0
+    count_days()
 
-    print count_days()
+    print max(visited.values())

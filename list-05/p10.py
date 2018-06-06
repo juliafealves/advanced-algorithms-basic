ba00459055cc2bbba66f9d4cbda6cba1969e5a91
juliafealves <juliafealves>
2018-06-06 01:04:33 -0300

    Solved problem: P10 - The World is a Theatre

--- list-05/p10.py
+++ list-05/p10.py
@@ -1,4 +1,21 @@
-from itertools import permutations
+# coding: utf-8
+def fatorial(n, array):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        array[n] = n * fatorial(n - 1, array)
+        return array[n]
 
+factoriais = [1] * 61
+fatorial(60, factoriais)
 n, m, t = map(int, raw_input().split())
-man = range(n)
+groups = 0
+b = 4
+g = t - b
+
+while g != 0:
+    groups += (factoriais[n]/(factoriais[b] * factoriais[n - b])) * (factoriais[m]/(factoriais[g] * factoriais[m - g]))
+    g -= 1
+    b += 1
+
+print groups

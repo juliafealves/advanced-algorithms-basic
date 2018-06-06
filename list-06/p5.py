6336ab508ef6139428033f2f79d9ee6fa44925bd
juliafealves <juliafealves>
2018-06-06 18:01:57 -0300

    Solved problem: P5 - DZY Loves Chessboard

--- list-06/p5.py
+++ list-06/p5.py
@@ -1,25 +1,27 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
+import sys
 GOOD = '.'
 BAD = '-'
 BLACK = 'B'
 WHITE = 'W'
 
+sys.setrecursionlimit(101 * 101)
 def set_adjacents(i, j):
     other_chessman = WHITE if chessboard[i][j] == BLACK else BLACK
 
-    if j + 1 < len(chessboard[0]) and chessboard[i][j + 1] == GOOD:
+    if j < m - 1 and chessboard[i][j + 1] == GOOD:
         chessboard[i][j + 1] = other_chessman
         set_adjacents(i, j + 1)
 
-    if i + 1 < len(chessboard) and chessboard[i + 1][j] == GOOD:
+    if i < n - 1 and chessboard[i + 1][j] == GOOD:
         chessboard[i + 1][j] = other_chessman
         set_adjacents(i + 1, j)
 
-    if i - 1 >= 0 and chessboard[i - 1][j] == GOOD:
+    if i > 0 and chessboard[i - 1][j] == GOOD:
         chessboard[i - 1][j] = other_chessman
         set_adjacents(i - 1, j)
 
-    if j - 1 >= 0 and chessboard[i][j - 1] == GOOD:
+    if j > 0 and chessboard[i][j - 1] == GOOD:
         chessboard[i][j - 1] = other_chessman
         set_adjacents(i, j - 1)
 
@@ -27,7 +29,7 @@ chessboard = []
 n, m = map(int, raw_input().split())
 
 for i in xrange(n):
-    chessboard.append(list(raw_input()))
+    chessboard.append(list(raw_input().strip()))
 
 for i in xrange(n):
     for j in xrange(m):

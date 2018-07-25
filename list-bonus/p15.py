79b7fd32edf362d2000229d688f0f8623dc00b40
juliafealves <juliafealves>
2018-07-25 19:21:09 -0300

    Solved problem: P15 - Lucky Ticket

--- list-bonus/p15.py
+++ list-bonus/p15.py
@@ -2,11 +2,11 @@
 n = int(raw_input())
 ticket = raw_input()
 digits = map(int, list(ticket))
-only_numbers = set(7, 4)
 
 p1 = digits[:n/2]
-p2 = digits[:n/2]
+p2 = digits[n/2:]
 
-if only_numbers.difference(set(p1)) == only_numbers: print 'YES'
+if len(set(digits).difference(set([7,4]))) == 0 or len(set(digits).difference(set([7]))) == 0 or len(set(digits).difference(set([4]))) == 0:
+    if sum(p1) == sum(p2): print 'YES'
+    else: print 'NO'
 else: print 'NO'
-print digits
